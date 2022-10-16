cdd19e399040f485ebde9dac6597cd76b9d95af7
MmSARGIN <melih.sargin.53@gmail.com>
2022-10-16 19:15:17 +0300

    Filterbar

---
+++ src/components/FilterBar.vue
@@ -0,0 +1,299 @@
+<template>
+    <div class="flex justify-center">
+        <form
+  class="relative mx-auto divide-y divide-gray-100 overflow-auto rounded-lg border border-gray-100 bg-white shadow-lg"
+>
+  <header class="px-6 py-4" aria-label="Filter University Courses">
+    <strong class="text-lg font-medium text-gray-900">
+      Sister Filters
+    </strong>
+
+    <p class="mt-1.5 text-sm text-gray-500">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+  </header>
+
+  <div class="flow-root h-auto overflow-y-auto px-6">
+    <div class="divide-y divide-gray-100">
+      <div class="py-8">
+        <fieldset>
+          <legend class="font-medium text-gray-900">Mezuniyet Durumu</legend>
+
+          <ul class="mt-6 grid gap-4 grid-cols-1">
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="radio"
+                  name="study_type"
+                  class="h-6 w-6 border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Okumus
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="radio"
+                  name="study_type"
+                  class="h-6 w-6 border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Cahil :)
+                </span>
+              </label>
+            </li>
+          </ul>
+        </fieldset>
+      </div>
+
+      <div class="py-8">
+        <fieldset>
+          <legend class="font-medium text-gray-900">Konum</legend>
+
+          <ul class="mt-6 grid grid-cols-1 gap-4">
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="location"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Butun Ilceler
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="location"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Uskudar
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="location"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Umraniye
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="location"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">Fatih</span>
+              </label>
+            </li>
+          </ul>
+        </fieldset>
+      </div>
+
+      <div class="py-8">
+        <fieldset>
+          <legend class="font-medium text-gray-900">Yetkinlikler</legend>
+
+          <ul class="mt-6 grid grid-cols-1 gap-4">
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="university"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Cocuk Gelisimi
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="university"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Psiko-Motor Beceri Egitimi
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="university"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Ingilizce Egitimi
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="university"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Keman Egitimi
+                </span>
+              </label>
+            </li>
+          </ul>
+        </fieldset>
+
+       
+      </div>
+
+      <div class="py-8">
+        <fieldset>
+          <legend class="font-medium">Mesai Saatleri</legend>
+
+          <ul class="mt-6 grid grid-cols-1 gap-4">
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="commitment"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Tam Zamanli
+                </span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="commitment"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">
+                  Yari Zamanli
+                </span>
+              </label>
+            </li>
+          </ul>
+        </fieldset>
+      </div>
+
+      <div class="py-8">
+        <fieldset>
+          <legend class="font-medium">Deneyim</legend>
+
+          <ul class="mt-6 grid grid-cols-1 gap-4">
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="rating"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">Abla gibi</span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="rating"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">Abla</span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="rating"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">Abla gibi abla</span>
+              </label>
+            </li>
+
+            <li>
+              <label class="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  name="rating"
+                  class="h-6 w-6 rounded-md border-gray-200"
+                />
+
+                <span class="text-sm font-medium text-gray-700">ANA</span>
+              </label>
+            </li>
+          </ul>
+        </fieldset>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex items-center justify-between px-6 py-4">
+    <input
+      type="reset"
+      value="Clear all"
+      class="rounded-lg bg-gray-50 px-5 py-3 text-sm font-medium text-gray-700"
+    />
+
+    <button
+      class="rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+      type="submit"
+    >
+      Show 300+ results
+    </button>
+  </div>
+</form>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    name: "FilterBar",
+  };
+  </script>
+  
--- src/views/BabySittersView.vue
+++ src/views/BabySittersView.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="flex flex-row flex-wrap gap">
+  <div class="grid grid-cols-8">
+    <div class="grid col-span-2">
+      <!--
+  This component uses @tailwindcss/forms
+
+  yarn add @tailwindcss/forms
+  npm install @tailwindcss/forms
+
+  plugins: [require('@tailwindcss/forms')]
+-->
+<FilterBar />
+
+
+    </div>
+    <div class="grid grid-cols-2  col-span-6 gap-6">
     <BabySitterCard />
     <BabySitterCard />
     <BabySitterCard />
@@ -8,14 +22,18 @@
     <BabySitterCard />
     <BabySitterCard />
   </div>
+  </div>
+  
 </template>
 
 <script>
-import BabySitterCard from "@/components/BabySitterCard.vue"
+import BabySitterCard from "@/components/BabySitterCard.vue";
+import FilterBar from "@/components/FilterBar.vue"
 
 export default {
   components: {
-    BabySitterCard
+    BabySitterCard,
+    FilterBar
   }
 }
 </script>
